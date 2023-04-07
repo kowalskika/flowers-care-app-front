1b8e46ccd5cae05ffb8e84fe47abbcca65a98a9b
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-07 16:49:20 +0200

    Added hook for forms validation.

---
+++ src/hooks/useFormValidation.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+
+const NAME_REGEX = /^.{3,100}$/;
+
+export const useFlowerValidation = ({
+  name,
+}: { name?: string }) => {
+  const [nameError, setNameError] = useState(false);
+
+  useEffect(() => {
+    if (name && !NAME_REGEX.test(name)) {
+      setNameError(true);
+    } else {
+      setNameError(false);
+    }
+  }, [name]);
+
+  return {
+    nameError,
+  };
+};
