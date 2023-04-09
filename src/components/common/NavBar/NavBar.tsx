164896673eaea7c40c5311b0256dc6020496f598
kowalskika <125073360+kowalskika@users.noreply.github.com>
2023-04-09 22:34:31 +0200

    Fixed navbar in login/register view.

--- src/components/common/NavBar/NavBar.tsx
+++ src/components/common/NavBar/NavBar.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SlSocialGithub } from 'react-icons/sl';
 
-import './NavBar.css';
 import { SettingButton } from '../SettingsButton/SettingsButton';
 import { AddButton } from '../AddButton/AddButton';
 import { GoBackButton } from '../GoBackButton/GoBackButton';
+import './NavBar.css';
 
 export function NavBar(props: { page: string }) {
   const { page } = props;
   return (
     <header className="NavBar__header">
-      <p className="NavBar__p-mobile-disable">Your Flowers Care</p>
+      <p className={page === 'login' ? ('NavBar__p-mobile-show') : 'NavBar__p-mobile-disable'}>Your Flowers Care</p>
       <nav>
         <ul className="NavBar__links">
           {page === 'main' && (<li><AddButton confirm={false} /></li>)}
